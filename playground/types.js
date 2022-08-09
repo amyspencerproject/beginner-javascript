8a9c3f653ba8b98a7c7f16062e051aa03207c6c6
amyspencerproject <amy@spencerproject.com>
2022-08-08 22:44:21 -0600

    Review of numbers

--- playground/types.js
+++ playground/types.js
@@ -1,37 +1,84 @@
 // console.log('Hooray it works!');
-// strings
+
+// STRINGS
 /* eslint-disable */
-const first = 'Amy';
-const middle = "C";
-const last = `Spencer`;
+// const first = 'Amy';
+// const middle = "C";
+// const last = `Spencer`;
 
-const string = "She's so cool";
-// const string2 = "She's so "cool"";
-const string2 = "She's so \"cool\" ";
-// const string3 = 'Shes so cool';
-const string3 = 'She\'s so cool';
-const string4 =`She's so "cool"`;
+// const string = "She's so cool";
+// // const string2 = "She's so "cool"";
+// const string2 = "She's so \"cool\" ";
+// // const string3 = 'Shes so cool';
+// const string3 = 'She\'s so cool';
+// const string4 =`She's so "cool"`;
 
-const song = ` I
-love
-pizza
+// const song = ` I
+// love
+// pizza
 
-oh yeah
+// oh yeah
 
-I do`;
+// I do`;
 
 // console.log(song);
 
-const hello = `Hello my name is ${first}. I am ${100 + 32} years old.`
+// const hello = `Hello my name is ${first}. I am ${100 + 32} years old.`
 
-const html = `
-<div>
-<h2>${first}</h2>
-<p>${hello}</p>
-</div>
-`;
+// const html = `
+// <div>
+// <h2>${first}</h2>
+// <p>${hello}</p>
+// </div>
+// `;
 
-document.body.innerHTML = html;
+// document.body.innerHTML = html;
+
+//NUMBERS
+
+// const integer = 100;
+// const float = 102.32;
+// const age = "12";
+// console.log(typeof integer);
+// console.log(typeof float);
+// console.log(typeof age);
+
+// const a = 10;
+// const b = 20;
+
+// console.log(a+"1"); // give 11 because of the + plus sign is loaded bc of concatenation
+
+// const round = Math.round(20.5); //rounds number
+// const floor = Math.floor(20.999999); //gives lower integer of float
+// const ceiling = Math.ceil(20.99999); //gives upper integer of float
+// const random = Math.random(); //gives a random float between 0 and 1
+
+// const candies = 20;
+// const kids = 3;
+// const eachKidGets = (candies/kids); //gives 6.666667 
+// const eachKidReallyGets = Math.floor(eachKidGets); //gives 6
+// const remainingCandies = candies - eachKidReallyGets; //gives 2
+// // or use the % module method to do division, round down, and return the whole integer remainder 
+// const dadGets = candies % kids; //gives 2 
+
+// console.log(window.location = `https://${0.1+0.2}.com`); //take you to floating point math website
+//vaguely remember this from NMR experiments. Be careful using decimals.
+
+//keep prices in cents until being displayed to avoid errors from floating point math
+// const price = 1034; //$10.34
+
+//infinity and negative infinity
+// const exponent = 10 ** 2; //returns 100
+// const exponentInfinity = 1000 ** 200 //returns infinity
+// console.log(type of exponentInfinity); //returns number
+
+// const notANumber = 10/"dog";
+// console.log(notANumber); //returns NaN
+// console.log(typeof notANumber); //returns number bc still catagorized as a number
+
+
+//BOOLEAN
+
 
 
 
